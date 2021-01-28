ecc793f26f5ef30836d2fbedd0d8eb508e83362e
Hector Ramos <hector.ramos@berkeley.edu>
2021-01-28 11:48:03 -0800

    fix 1.0

--- src/App.js
+++ src/App.js
@@ -13,32 +13,33 @@ import Preferences from './pages/Preferences';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/SignIn">
-          <SignIn />
-        </Route>
-        <Route path="/Register">
-          <Register />
-        </Route>
-        <Route path="/Main">
-          <Main />
-        </Route>
-        <Route path="/Profile">
-          <Profile />
-        </Route>
-        <Route path="/Preferences">
-          <Preferences />
-        </Route>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Route default> 
-        {/* Default isn't working for some reason, need to fix it. */}
-          <NotFound />
-        </Route>
-      </Switch>
-  </Router>
+    <h1>hiiiiiiiiii Jennifer</h1>
+  //   <Router>
+  //     <Switch>
+  //       <Route path="/SignIn">
+  //         <SignIn />
+  //       </Route>
+  //       <Route path="/Register">
+  //         <Register />
+  //       </Route>
+  //       <Route path="/Main">
+  //         <Main />
+  //       </Route>
+  //       <Route path="/Profile">
+  //         <Profile />
+  //       </Route>
+  //       <Route path="/Preferences">
+  //         <Preferences />
+  //       </Route>
+  //       <Route exact path="https://designsandmoredesigns.com/">
+  //         <Landing />
+  //       </Route>
+  //       <Route default> 
+  //       {/* Default isn't working for some reason, need to fix it. */}
+  //         <NotFound />
+  //       </Route>
+  //     </Switch>
+  // </Router>
   );
 };
   
